0d84422e6af13520f95f6596db18f9c6e0f49b02
jvmahon <jvm33@columbia.edu>
2021-04-30 16:18:45 -0400

    Update testConfigurationBulkGet.groovy

--- Archive/testConfigurationBulkGet.groovy
+++ Archive/testConfigurationBulkGet.groovy
@@ -9,6 +9,8 @@ metadata {
     definition (name: "Testing Configuration Bulk Get", namespace: "jvm", author:"jvm") {
 
         capability "Configuration"
+        command "getBulk"
+        command "getIndividual"
     }
 }
 
@@ -25,12 +27,20 @@ log.debug "Configuration Buk Report ${cmd}"
 
 void getBulk()
 {
-    List<hubitat.zwave.Command> cmds = []
+    log.debug "Sending a bulk get"
 
-    cmds.add(zwave.configurationV2.configurationBulkGet(numberOfParameters:4, parameterOffset: 11))
-    sendToDevice( cmds )
+    sendToDevice( secure(zwave.configurationV2.configurationBulkGet(numberOfParameters:10, parameterOffset: 1)))
 }
+void getIndividual()
+{
+    log.debug "Sending individual gets"
 
+    Short param = 0
+    for( param  = 1; param <= 12; param++)
+    {
+    sendToDevice( secure(zwave.configurationV2.configurationGet(parameterNumber:param as Short)))
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 //////        Handle Startup and Configuration Tasks           ///////
@@ -54,8 +64,8 @@ void zwaveEvent(hubitat.zwave.commands.securityv1.SecurityMessageEncapsulation c
 }
 
 void parse(String description) {
-    log.debug "parse:${description}"
-    hubitat.zwave.Command cmd = zwave.parse(description, CMD_CLASS_VERS)
+    // log.debug "parse:${description}"
+    hubitat.zwave.Command cmd = zwave.parse(description, [0x70:1])
     if (cmd) {
         zwaveEvent(cmd)
     }
@@ -66,30 +76,17 @@ void sendToDevice(List<hubitat.zwave.Command> cmds) {
 }
 
 void sendToDevice(hubitat.zwave.Command cmd) {
-    sendHubCommand(new hubitat.device.HubAction(secureCommand(cmd), hubitat.device.Protocol.ZWAVE))
+    sendHubCommand(new hubitat.device.HubAction(cmd, hubitat.device.Protocol.ZWAVE))
 }
 
 void sendToDevice(String cmd) {
-    sendHubCommand(new hubitat.device.HubAction(secureCommand(cmd), hubitat.device.Protocol.ZWAVE))
+    sendHubCommand(new hubitat.device.HubAction(cmd, hubitat.device.Protocol.ZWAVE))
 }
 
 List<String> commands(List<hubitat.zwave.Command> cmds, Long delay=200) {
     return delayBetween(cmds.collect{ secureCommand(it) }, delay)
 }
 
-String secureCommand(hubitat.zwave.Command cmd) {
-    secureCommand(cmd.format())
-}
-
-String secureCommand(String cmd) {
-    String encap=""
-    if (getDataValue("zwaveSecurePairingComplete") != "true") {
-        return cmd
-    } else {
-        encap = "988100"
-    }
-    return "${encap}${cmd}"
-}
 
 String secure(String cmd){
     return zwaveSecureEncap(cmd)
@@ -99,6 +96,16 @@ String secure(hubitat.zwave.Command cmd){
     return zwaveSecureEncap(cmd)
 }
 
+void zwaveEvent(hubitat.zwave.commands.configurationv1.ConfigurationReport cmd) {
+   //  log.debug "Received a configuratino Report"
+    log.debug "${cmd}"
+}
+
+void zwaveEvent(hubitat.zwave.commands.configurationv2.ConfigurationReport cmd) {
+   //  log.debug "Received a configuratino Report"
+    log.debug "${cmd}"
+}
+
 void zwaveEvent(hubitat.zwave.Command cmd) {
-    if (logEnable) log.debug "skip:${cmd}"
+    log.debug "${cmd}"
 }
